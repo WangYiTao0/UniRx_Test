66b9072eb07c3c336c3d06a09b0454fd9717fea5
Wang <as5818956@gmail.com>
2021-12-30 00:17:07 +0900

    初始化项目

---
+++ Assets/Scripts/TestScripts.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mono.Cecil;
+using UnityEngine;
+
+public class TestScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       // StartCoroutine();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
